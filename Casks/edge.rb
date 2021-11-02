393d6b5435eeb3e6bc7bb28df9836c95598a6170
Hai Nguyen <hai.nguyen@juve.vn>
2021-11-02 13:59:28 +0700

    Initial commit

---
+++ Casks/edge.rb
@@ -0,0 +1,35 @@
+cask "microsoft-edge" do
+  folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
+  linkid = Hardware::CPU.intel? ? "2069148" : "2093504"
+
+  version "95.0.1020.40"
+
+  if Hardware::CPU.intel?
+    sha256 "cc865bc4848cbe07a318cfce1df7f6586a0af2ca99e3c5c87b057ec1cc6845fc"
+  else
+    sha256 "9a73d1970656a7f7a3ba654a892a7085adaedefa566e32340bfdbffb1191faa2"
+  end
+
+  url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdge-#{version}.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/"
+  name "Microsoft Edge"
+  desc "Web browser"
+  homepage "https://www.microsoft.com/edge"
+
+  livecheck do
+    url "https://go.microsoft.com/fwlink/?linkid=#{linkid}"
+    strategy :header_match
+  end
+
+  auto_updates true
+
+  uninstall pkgutil: "com.microsoft.edgemac"
+
+  zap trash: [
+    "~/Library/Application Support/Microsoft Edge",
+    "~/Library/Caches/Microsoft Edge",
+    "~/Library/Preferences/com.microsoft.edgemac.plist",
+    "~/Library/Saved Application State/com.microsoft.edgemac.savedState",
+  ],
+      rmdir: "/Library/Application Support/Microsoft"
+end
